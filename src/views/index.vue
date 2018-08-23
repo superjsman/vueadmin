b70fe2b8ba8f5f395294083515162a4e135b40a9
superjsman <sousuo.love@qq.com>
2018-08-23 22:31:10 +0800

    首页侧边栏完成

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,94 @@
 <template>
-    <div class="index">
-        index页面
+    <div class="home">
+        <el-container>
+            <el-aside width="200px">
+                <div class="logo"></div>
+ <el-menu
+      default-active="2"
+      class="el-menu-admin"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item-group>          
+          <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+         </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
+<script>
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  background-color: #E5E5E5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+    overflow-x: hidden;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #009688;
+  }
+  .logo {
+    height:60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: white;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: white;
+    cursor: pointer;
+    &:hover {
+      background-color: #00635a;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome, {
+    color: white;
+  }
+}
+</style>
+
