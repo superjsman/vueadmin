d18ca7f089169f4779f986b2220085445c49c856
superjsman <sousuo.love@qq.com>
2018-08-26 12:10:19 +0800

    完成添加商品分类功能

--- src/views/goods/categories.vue
+++ src/views/goods/categories.vue
@@ -9,6 +9,9 @@
                 </el-breadcrumb>
             </el-col>
         </el-row>
+        <el-row>
+            <el-col :span="24"> <el-button type="success" plain @click="addCat">添加分类</el-button></el-col>
+        </el-row>
         <tree-grid
         :treeStructure="true"
         :columns="columns"
@@ -27,11 +30,33 @@
         layout="total, sizes, prev, pager, next, jumper"
         :total="total">
         </el-pagination>
+        <el-dialog title="添加分类" :visible.sync="addFormVisible">
+        <el-form :model="addForm" label-width="80px" ref="addForm" :rules="addreles">
+            <el-form-item label="分类名称" prop="catName">
+            <el-input v-model="addForm.catName" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="选择分类">
+                <el-cascader
+                :props="props"
+                :options="options"
+                placeholder="请选择分类"
+                :show-all-levels="false"
+                v-model="selectData"
+                @change="handleChange">
+                </el-cascader>
+            </el-form-item>
+           
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="addFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="addCatSure">确 定</el-button>
+        </div>
+        </el-dialog>
     </div>
 </template>
 <script>
 import TreeGrid from '@/components/TreeGrid/TreeGrid'
-import {goodCat} from '@/api'
+import {goodCat, CatById, addCat} from '@/api'
     export default {
         data () {
             return {
@@ -53,7 +78,23 @@ import {goodCat} from '@/api'
                 pagenum: 1,
                 pagesize: 10,
                 total: 0,
-                loading: false
+                loading: false,
+                addFormVisible: false,
+                addForm: {
+                    catName: '',                    
+                    },
+                options: [],
+                selectData: [],
+                 props: {
+                        value: 'cat_id',
+                        label: 'cat_name',
+                        children: 'children'
+                    },
+                catId: 0,
+                level: 0,
+                addreles: {
+                    catName: [{required: true, message: '分类名称不能为空', trigger: 'blur'}]
+                }
             }
         },
         methods: {
@@ -73,7 +114,7 @@ import {goodCat} from '@/api'
             editCategory (cid) {
                 console.log(cid)
             },
-            getlist(){
+            getlist(){ //获取数据
                 this.loading = true
                 goodCat(3).then(res => {
                     console.log(res)
@@ -89,6 +130,47 @@ import {goodCat} from '@/api'
                     this.dataSource = arr
                     this.loading = false
                 })
+            },
+            addCat() { //选择框数据初始化
+                let that = this
+                this.addFormVisible = true
+                goodCat(2).then(res => {
+                    this.options = res.data
+                    })
+            },
+             handleChange(value) { //添加分类选择框变化函数
+                console.log(value);
+                this.catId = value[value.length-1],
+                CatById(this.catId).then(res => {
+                    if(res.meta.status === 200) {
+                       this.level = res.data.cat_level
+                    }
+                })
+                
+                            },
+            addCatSure(){
+                if (this.catName === '') {
+                    this.$message({
+                        message: '操作失败!请填写完整信息!',
+                        type: 'error'
+                    })
+                } else {
+                    this.addFormVisible = false                    
+                    addCat({
+                        cat_pid: this.catId,
+                        cat_name: this.addForm.catName,
+                        cat_level: this.level
+                    }).then(res => {
+                        if(res.meta.status === 201){
+                            this.$message({
+                                message: res.meta.msg,
+                                type: 'success'
+                            })
+                            this.getlist()
+                            this.addForm.catName = ''
+                        }
+                    })
+                }
             }
         },
         components: {
